a67c2db36ed672fecc4c0ed4511b8c7903acb23c
Rohit Modanwal <77rohitmodanwal0@gmail.com>
2025-07-30 21:11:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Login from './components/Login';
+const App= ()=>{
+  return<>
+ <div className='grid w-full h-screen place-items-center bg-cyan-400'>
+  <Login />
+</div>
+
+  </>
+}
+export default App;
